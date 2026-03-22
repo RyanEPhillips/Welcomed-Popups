9e811ccae00168e04f67ce43426ad86f2ebe76fc
2085Anony420 <85RyanP@gmail.com>
2026-03-21 20:40:02 -0400

    seperate image and message functions for functinality cleaned up a few things

--- Random.py
+++ Random.py
@@ -11,55 +11,57 @@ messages = [
     "You're awesome!",
     "Don't give up!",
     "You've got this!",
-    "Stay positive!"
+    "Stay positive!",
     "You are capable!",
     "You are strong!",
     "You are enough!"
 ]
 
 images = [
-    "1.jpg",
-    "2.jpg",
-    "3.jpg",
-    "4.jpg",
-    "5.jpg",
-    "6.jpg",
-    "7.jpg",
-    "8.jpg",
-    "9.jpg",
-    "10.jpg"
+    "img/1.jpg",
+    "img/2.jpg",
+    "img/3.jpg",
+    "img/4.png",
+    "img/5.png",
+    "img/6.jpg",
+    "img/7.jpg"
 ]
 
 
-def show_me(msg):
+def show_message(msg):
     root = tk.Tk()
-    root.withdraw()  # Hide the main window
-    messagebox.showinfo("Message", msg)
+    root.withdraw()
+    messagebox.showinfo("Reminder", msg)
+    root.destroy()
 
-    msg = random.choice(messages)
-    img_path = random.choice(images)
+
+def show_image(img_path):
+    root = tk.Tk()
+    root.title("Reminder")
 
     img = Image.open(img_path)
     img = img.resize((300, 300))
     photo = ImageTk.PhotoImage(img)
 
-    label_img = tk.Label(root, image=photo)
-    label_img.pack()
+    label = tk.Label(root, image=photo)
+    label.image = photo  # keep reference
+    label.pack()
 
-    label_text = tk.Label(root, text=msg, font=("Times New Roman", 16))
-    label_text.pack()
-
-    button = tk.Button(root, text="Close", command=root.destroy)
-    button.pack()
-
-    time.sleep(5)  # Keep the message box open for 5 seconds
-    root.destroy()  # Close the message box after displaying
+    # auto close after 5 seconds
+    root.after(5000, root.destroy)
 
     root.mainloop()
 
+
 while True:
     wait_time = random.randint(10, 30)
-    # wait_time = random.randint(900, 1800) # Random wait time between 15 and 30 minutes (900 to 1800 seconds)
     time.sleep(wait_time)
-    show_me(msg=random.choice(messages))
 
+    choice = random.choice(["message", "image"])
+
+    if choice == "message":
+        msg = random.choice(messages)
+        show_message(msg)
+    else:
+        img = random.choice(images)
+        show_image(img)
